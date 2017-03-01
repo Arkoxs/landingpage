319e260f2f8f5c8feb77392a1f283f907e68076b
Arkoxs <arko@twistvlietpad.nl>
2017-03-01 22:04:22 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" class="gr__landingpages_cloudapp_net">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+		<title>Dit domein is al geregistreerd</title>
+		<style type="text/css">
+		<!--
+		html, body {
+			height: 100%;
+			margin: 0;
+			padding: 0;
+			overflow: hidden;
+		}
+		
+		body {
+			font: 75%/2 Arial, Helvetica, sans-serif;
+			color: #3B3B3B;
+			background: #FFFFFF;
+			border-top: 21px solid #3B3B3B;
+		}
+		
+		h1 {
+			margin: 0 0 21px 0;
+			padding: 0;
+		}
+		
+		a {
+			text-decoration: none;
+			color: #009EDF;
+		}
+		
+		#message {
+			width: 640px;
+			height: 140px;
+			position: absolute;
+			top: 50%;
+			left: 50%;
+			margin: -70px 0 0 -320px;
+		}
+		
+		#logo {
+			position: absolute;
+			right: 1px;
+		}
+		
+		#adwords {
+			text-align: center;
+			position: absolute;
+			bottom: 0;
+			width: 100%;
+			padding: 42px 0 84px;
+			border-top: 1px solid #E5E5E5;
+		}
+
+		html>body strong:after { content: "domains@twistvlietpad.nl"; }
+
+		html>body strong span { display: none; }
+
+		-->
+		</style>
+	</head>
+	<body data-gr-c-s-loaded="true">
+		<div id="message">
+			<h1><img src="dit_domein_is_geregistreerd.png" width="519" height="68" border="0" alt="Dit domein is al geregistreerd."></h1>
+				<img id="logo" src="logo_dockwork.png" width="116" height="21" border="0" alt="Dit domein is al geregistreerd.">
+		</div>
+		<div id="adwords">
+			<script src="./index_files/ca-pub-4418310345798859.js.download"></script><script type="text/javascript"><!--
+				google_ad_client = "pub-4418310345798859";
+				/* 468x15, gemaakt 21-1-09 */
+				google_ad_slot = "3180617257";
+				google_ad_width = 468;
+				google_ad_height = 15;
+				//-->
+			</script>
+			<script type="text/javascript" src="./index_files/show_ads.js.download"></script><ins id="aswift_0_expand" style="display:inline-table;border:none;height:15px;margin:0;padding:0;position:relative;visibility:visible;width:468px;background-color:transparent"><ins id="aswift_0_anchor" style="display:block;border:none;height:15px;margin:0;padding:0;position:relative;visibility:visible;width:468px;background-color:transparent"><iframe width="468" height="15" frameborder="0" marginwidth="0" marginheight="0" vspace="0" hspace="0" allowtransparency="true" scrolling="no" allowfullscreen="true" onload="var i=this.id,s=window.google_iframe_oncopy,H=s&amp;&amp;s.handlers,h=H&amp;&amp;H[i],w=this.contentWindow,d;try{d=w.document}catch(e){}if(h&amp;&amp;d&amp;&amp;(!d.body||!d.body.firstChild)){if(h.call){setTimeout(h,0)}else if(h.match){try{h=s.upd(h,i)}catch(e){}w.location.replace(h)}}" id="aswift_0" name="aswift_0" style="left:0;position:absolute;top:0;" src="./index_files/saved_resource.html"></iframe></ins></ins>
+		</div>
+	
+	</body>
+</html>
