dd9d8395d7e3b6396bfefe51670b96cf5538ced1
Arkoxs <arko@twistvlietpad.nl>
2017-03-01 22:11:43 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
+
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" class="gr__landingpages_cloudapp_net">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<html xmlns="http://www.w3.org/1999/xhtml">
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 		<title>Dit domein is al geregistreerd</title>
 		<style type="text/css">
 		<!--
@@ -59,13 +60,13 @@
 		-->
 		</style>
 	</head>
-	<body data-gr-c-s-loaded="true">
+	<body>
 		<div id="message">
-			<h1><img src="dit_domein_is_geregistreerd.png" width="519" height="68" border="0" alt="Dit domein is al geregistreerd."></h1>
-				<img id="logo" src="logo_dockwork.png" width="116" height="21" border="0" alt="Dit domein is al geregistreerd.">
+			<h1><img src="dit_domein_is_geregistreerd.png" width="519" height="68" border="0" alt="Dit domein is al geregistreerd." /></h1>
+				<img id="logo" src="logo_dockwork.png" width="116" height="21" border="0" alt="Dit domein is al geregistreerd." />
 		</div>
 		<div id="adwords">
-			<script src="./index_files/ca-pub-4418310345798859.js.download"></script><script type="text/javascript"><!--
+			<script type="text/javascript"><!--
 				google_ad_client = "pub-4418310345798859";
 				/* 468x15, gemaakt 21-1-09 */
 				google_ad_slot = "3180617257";
@@ -73,8 +74,7 @@
 				google_ad_height = 15;
 				//-->
 			</script>
-			<script type="text/javascript" src="./index_files/show_ads.js.download"></script><ins id="aswift_0_expand" style="display:inline-table;border:none;height:15px;margin:0;padding:0;position:relative;visibility:visible;width:468px;background-color:transparent"><ins id="aswift_0_anchor" style="display:block;border:none;height:15px;margin:0;padding:0;position:relative;visibility:visible;width:468px;background-color:transparent"><iframe width="468" height="15" frameborder="0" marginwidth="0" marginheight="0" vspace="0" hspace="0" allowtransparency="true" scrolling="no" allowfullscreen="true" onload="var i=this.id,s=window.google_iframe_oncopy,H=s&amp;&amp;s.handlers,h=H&amp;&amp;H[i],w=this.contentWindow,d;try{d=w.document}catch(e){}if(h&amp;&amp;d&amp;&amp;(!d.body||!d.body.firstChild)){if(h.call){setTimeout(h,0)}else if(h.match){try{h=s.upd(h,i)}catch(e){}w.location.replace(h)}}" id="aswift_0" name="aswift_0" style="left:0;position:absolute;top:0;" src="./index_files/saved_resource.html"></iframe></ins></ins>
+			<script type="text/javascript" src="http://pagead2.googlesyndication.com/pagead/show_ads.js"></script>
 		</div>
-	
 	</body>
 </html>
